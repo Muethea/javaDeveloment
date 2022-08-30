af3cc9f673d0c2fb4047dc06d0f056d83190ce59
Fernando dos Santos Muethea <64161845+Muethea@users.noreply.github.com>
2022-08-30 09:55:17 +0100

    Primeiro Programa

--- src/javaDeveloment/teste.java
+++ src/javaDeveloment/teste.java
@@ -1,10 +1,40 @@
 package javaDeveloment;
 
+import java.util.Scanner;
+
+/**
+ * @author Fernando Muethea
+ *
+ */
 public class teste {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Scanner in = new Scanner (System.in);
+		
+		String name, sobrename, apelido, endereco;
+		int age;
+		int idadePermitida =18;
+		System.out.println("Digite seu nome: ");
+		name = in.nextLine();
+		
+		System.out.println("Digite seu Sobrenome: ");
+		sobrename = in.nextLine();
+		
+		System.out.println("Digite seu apelido: ");
+		apelido = in.nextLine();
+		
+		System.out.println("Digite seu endereco: ");
+		endereco = in.nextLine();
+		
+		System.out.println("Qual é sua idade? ");
+		age = in.nextInt();
+		
+		if (age >= idadePermitida) {
+			System.out.println("Ola "+name+" ja podes votar :)");
+		}else {
+			System.out.println("OLa "+name+" Nao podes votar pois a sua idade é menor que "+idadePermitida);
+		}
+		
 	}
-
 }
